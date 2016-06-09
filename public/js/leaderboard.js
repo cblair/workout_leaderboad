7bcc977f7639c10df4caaca22793969217295e10
cblair <colby.blair@gmail.com>
2016-06-09 07:42:25 -0700

    fixing directive issue

--- public/js/leaderboard.js
+++ public/js/leaderboard.js
@@ -1,3 +1,19 @@
 (function () {
-    
-});
+    // ### leaderboard
+    // Our leaderboard module.
+    var app = angular.module('leaderboard', []);
+
+    // ### table
+    // Table directive of workouts to display.
+    app.directive('leaderboard', function () {
+        console.log("TS2");
+        return {
+            restrict: 'E',
+            templateUrl: '/leaderboard/table.html',
+            controller: function () {
+            
+            },
+            controllerAs: 'table'
+        };
+    });
+})();
