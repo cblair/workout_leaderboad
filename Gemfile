f7c24c060e047a08270806e7b568b810b77d3fb1
cblair <colby.blair@gmail.com>
2016-06-08 20:32:51 -0700

    stubbing main page

---
+++ Gemfile
@@ -0,0 +1,6 @@
+# Gemfile for our Rack app.
+source "https://rubygems.org"
+
+ruby '2.2.4'
+
+gem 'rack'
---
+++ public/js/index.js
@@ -0,0 +1,5 @@
+// # Index
+// JS for all common site functionality.
+
+// Initialize foundation when the DOM is ready.
+$(function(){ $(document).foundation(); });
