25089aca174441b2eb7f42f8d620dc6d44851965
cblair <colby.blair@gmail.com>
2016-06-09 22:15:35 -0700

    getting data with some basic stubs

--- public/js/leaderboard.js
+++ public/js/leaderboard.js
@@ -1,19 +1,90 @@
+'use strict';
+
 (function () {
+    const LEADERBOARD_API_URL = 
+        'https://apis.trainheroic.com/public/leaderboard/468425';
+
     // ### leaderboard
     // Our leaderboard module.
     var app = angular.module('leaderboard', []);
 
-    // ### table
-    // Table directive of workouts to display.
-    app.directive('leaderboard', function () {
-        console.log("TS2");
+    // ### leaderboard
+    // Leaderboard table directive of workouts to display.
+    app.directive('leaderboard', ['$http', function ($http) {
         return {
             restrict: 'E',
             templateUrl: '/leaderboard/table.html',
             controller: function () {
-            
+                // Create a closure reference for our Promises.
+                var leaderboardCtl = this;
+
+                // ### leaderboardData
+                // List of all the data point in the leader board.
+                this.leaderboardData = [];
+                
+                // ### getLeaderboardData
+                // Get leaderboard data from the API. This function exposes
+                // the data as segments, which sizes are limited. Similar to
+                // most pagination schemes. For example, if total data size
+                // is 200, settings size to 10 gives 20 segments. Asking for
+                // segment 1 gives points 0-9, 2 gives 10-19, etc.
+                //
+                // #### Params
+                // `segment` (Number) - which nth segement to get. 0 based.
+                // `size` (Number) - the size of the segments.
+                //
+                // #### Returns
+                // (Promise) A promise to be fullfilled when the data request
+                // has a response.
+                //
+                // #### Enhancements
+                // Find out what params the API can take so it segments the
+                // data for us.
+                this.getLeaderboardData = function (segment, size) {
+                    // TODO - the right way would be to pass these params to
+                    // the API, which hopefully takes similar parameters as this
+                    // function. We'll fake this for now, ineffeciently getting
+                    // all the data, until we learn more about the API.
+                    return $http.get(LEADERBOARD_API_URL
+                        // TODO - here's where we should put API params.
+                        )
+                        .then(
+                            function success(response) {
+                                var data = response &&
+                                    response.data &&
+                                    response.data.results &&
+                                    Array.isArray(response.data.results) ?
+                                        response.data.results : []
+
+                                return leaderboardCtl.getSegmentedData(data,
+                                    segment, size);
+                            },
+                            function error(response) {
+                                return ('error');
+                            }
+                        );
+                };
+
+                this.getSegmentedData = function (data, segment, size) {
+                    return new Promise(function(fullfill, reject) {
+                        var from = segment * size,
+                            to = from + size,
+                            segmentData = data.slice(from, to);
+                        console.log(from);
+                        console.log(to);
+
+                        fullfill(segmentData);
+                    });
+                };
+
+                var result = this.getLeaderboardData(1, 10)
+                    .then(function (data) {
+                        console.log(data);
+                        leaderboardCtl.leaderboardData = data;
+                    });
+
             },
-            controllerAs: 'table'
+            controllerAs: 'leaderboardCtl'
         };
-    });
+    }]);
 })();
